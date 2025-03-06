46795a2cb75e336d3a0211d5c9d3de94c9cad467
zaid2899 <zk9128782@gmail.com>
2025-03-06 18:10:29 +0530

    New project

--- account.py
+++ account.py
@@ -1,19 +1,19 @@
 # This project demonstrates the working of a bank with single account holder
 class BankAccount:
     def __init__(self,account_number,account_holder,balance):
-        self.account_number = account_number
-        self.account_holder = account_holder
-        self.balance = balance
+        self.account_number = account_number   # Account_number should be integer
+        self.account_holder = account_holder   # Account_holder should contain name of holder 
+        self.balance = balance                 # Balance should contain amount contained in the account
         
     def deposit(self,depositAmount):
-        self.depositAmount = depositAmount
+        self.depositAmount = depositAmount     # Amount deposited should be added to the balance of the account
         if self.depositAmount < 0:
             print("Enter valid amount")
         else:
             self.balance = self.balance + self.depositAmount
     
     def withdraw(self,withdrawAmount):
-        self.withdrawAmount = withdrawAmount
+        self.withdrawAmount = withdrawAmount  # amount withdrawn should be subtracted from the balance of the account
         if self.withdrawAmount > self.balance:
             print("Enter valid amount")
         else:
